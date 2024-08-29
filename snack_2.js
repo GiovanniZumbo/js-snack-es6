15030a812e4b548b11e7fac5e7d3c490c4367d75
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-29 16:48:21 +0200

    snack_2 (last commit)

--- snack_2.js
+++ snack_2.js
@@ -12,36 +12,54 @@ const students = [
         id: 213,
         fullName: 'Marco della Rovere',
         grades: 78
-    }
+    },
     {
         id: 110,
         fullName: 'Paola Cortellessa',
         grades: 96
-    }
+    },
     {
         id: 250,
         fullName: 'Andrea Mantegna',
         grades: 48
-    }
+    },
     {
         id: 145,
         fullName: 'Gaia Borromini',
         grades: 74
-    }
+    },
     {
         id: 196,
         fullName: 'Luigi Grimaldello',
         grades: 68
-    }
+    },
     {
         id: 102,
         fullName: 'Piero della Francesca',
         grades: 50
-    }
+    },
     {
         id: 120,
         fullName: 'Francesca da Polenta',
         grades: 84
     }
 
-]
+];
+
+// * Step 1
+
+const studentsNames = students.map(student => student.fullName.toUpperCase());
+
+console.log(studentsNames);
+
+// * Step 2
+
+const gradesOver70 = students.filter(student => student.grades > 70);
+
+console.log(gradesOver70);
+
+// * Step 3
+
+const gradesOver70IdOver120 = students.filter(student => student.grades > 70 && student.id > 120);
+
+console.log(gradesOver70IdOver120);
