39021d703462e9cab934d6cb7a19101fa31ebcba
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-29 15:53:52 +0200

    Snack 1

--- script.js
+++
@@ -1,9 +0,0 @@
-// PLANNING
-
-/* */
-
-// # STAGING
-// # DATA GATHERING
-// # EVENT HANDLING
-// # PROCESSING
-// # OUTPUT 
---
+++ snack_1.js
@@ -0,0 +1,35 @@
+// PLANNING
+
+/* *SNACK 1*
+Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+```
+[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+```
+Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto javascript che ha come proprietà:
+nome del tavolo (tableName),
+nome dell'ospite (guestName),
+posto occupato (place),
+Generiamo e stampiamo in console la lista per i segnaposto. */
+
+
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+
+// Creare un array lista con il metodo .map
+const guestList = guests.map((guest, i) => (
+
+    // Inserire per ogni invitato, un oggetto che dovrà avere tableName, guestName e place
+    {
+        tableName: "Tavolo Vip",
+        guestName: `${guests[i]}`,
+        place: `${i + 1}`
+    }
+
+))
+
+console.log(guestList);
+
+// Stampare il nuovo array
+
+
+
